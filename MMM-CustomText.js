27d19350d0d10b917aa1a250a484768bb05fc0c8
dathbe <github@beffa.us>
2024-03-30 11:49:58 -0700

    Create MMM-CustomText.js

---
+++ MMM-CustomText.js
@@ -0,0 +1,52 @@
+/* global Log, Module */
+
+/* MagicMirror²
+ * Module: MMM-CustomText
+ *
+ * By dathbe
+ * CC Licensed.
+ */
+
+Module.register("MMM-CustomText", {
+
+	// Default config.
+	defaults: {
+		animationSpeed: 2 * 1000,
+		uniqueID: null,
+		initialMessage: "No notification received yet"
+	},
+
+	// Define required scripts.
+	getStyles () {
+		return ["MMM-CustomText.css"];
+	},
+
+	// Define start sequence.
+	start () {
+		this.notification = false;
+		this.messageText = this.config.initialMessage;
+	},
+
+	// dom generator.
+	getDom () {
+		const self = this;
+		// create html
+		const wrapper = document.createElement("div");
+		wrapper.className = "CustomTextDiv"
+		wrapper.innerHTML = self.messageText;
+		if (self.messageText == "") {
+			wrapper.style.display = 'none';
+		}
+		return wrapper;
+	},
+
+	notificationReceived (notification, payload, sender) {
+		if(notification === "CUSTOMTEXT_UPDATE" && (payload.uniqueID == this.config.uniqueID || !this.config.uniqueID)) {
+			Log.debug(`Received notification: ${notification} with payload.message: ${payload.message} from sender: ${sender}`);
+			this.messageText = payload.message;
+			this.notification = true;
+			this.updateDom(this.config.animationSpeed);
+		}
+	},
+
+});
