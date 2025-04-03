6cf4c448ea5aca102c271aca2fcfd7e2a575edd3
dathbe <github@beffa.us>
2025-04-02 21:48:11 -0700

    ESLint

--- MMM-CustomText.js
+++ MMM-CustomText.js
@@ -1,5 +1,3 @@
-/* global Log, Module */
-
 /* MagicMirror²
  * Module: MMM-CustomText
  *
@@ -7,46 +5,46 @@
  * MIT Licensed.
  */
 
-Module.register("MMM-CustomText", {
+Module.register('MMM-CustomText', {
 
-	// Default config.
-	defaults: {
-		animationSpeed: 2 * 1000,
-		uniqueID: null,
-		initialMessage: "No notification received yet"
-	},
+  // Default config.
+  defaults: {
+    animationSpeed: 2 * 1000,
+    uniqueID: null,
+    initialMessage: 'No notification received yet',
+  },
 
-	// Define required scripts.
-	getStyles () {
-		return ["MMM-CustomText.css"];
-	},
+  // Define required scripts.
+  getStyles() {
+    return ['MMM-CustomText.css']
+  },
 
-	// Define start sequence.
-	start () {
-		this.notification = false;
-		this.messageText = this.config.initialMessage;
-	},
+  // Define start sequence.
+  start() {
+    this.notification = false
+    this.messageText = this.config.initialMessage
+  },
 
-	// dom generator.
-	getDom () {
-		const self = this;
-		// create html
-		const wrapper = document.createElement("div");
-		wrapper.className = "CustomTextDiv"
-		wrapper.innerHTML = self.messageText;
-		if (self.messageText == "") {
-			wrapper.style.display = 'none';
-		}
-		return wrapper;
-	},
+  // dom generator.
+  getDom() {
+    const self = this
+    // create html
+    const wrapper = document.createElement('div')
+    wrapper.className = 'CustomTextDiv'
+    wrapper.innerHTML = self.messageText
+    if (self.messageText == '') {
+      wrapper.style.display = 'none'
+    }
+    return wrapper
+  },
 
-	notificationReceived (notification, payload, sender) {
-		if(notification === "CUSTOMTEXT_UPDATE" && (payload.uniqueID == this.config.uniqueID || !this.config.uniqueID)) {
-			Log.debug(`Received notification: ${notification} with payload.message: ${payload.message} from sender: ${sender}`);
-			this.messageText = payload.message;
-			this.notification = true;
-			this.updateDom(this.config.animationSpeed);
-		}
-	},
+  notificationReceived(notification, payload, sender) {
+    if (notification === 'CUSTOMTEXT_UPDATE' && (payload.uniqueID == this.config.uniqueID || !this.config.uniqueID)) {
+      Log.debug(`Received notification: ${notification} with payload.message: ${payload.message} from sender: ${sender}`)
+      this.messageText = payload.message
+      this.notification = true
+      this.updateDom(this.config.animationSpeed)
+    }
+  },
 
-});
+})
